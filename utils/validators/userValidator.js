cbcd00f8aee434d2ace2e30b712a4dce928a6fa0
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-05 11:55:11 +0600

    add user validator

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -1,5 +1,4 @@
 const { Joi } = require('celebrate');
-const { urlTemplate } = require('../constants');
 
 module.exports = {
   createOrLogin: {
@@ -7,19 +6,12 @@ module.exports = {
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8).max(35),
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(urlTemplate),
     }),
   },
   updateUser: {
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  },
-  updateAvatar: {
-    body: Joi.object().keys({
-      avatar: Joi.string().pattern(urlTemplate),
+      email: Joi.string().email(),
     }),
   },
   userID: {
