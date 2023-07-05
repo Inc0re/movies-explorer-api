51b0fcd071fa69c41c23ad30fcf1cbd08d9c74a9
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-05 11:54:28 +0600

    add user model

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { UnauthorizedError } = require('../utils/errors');
+
+const userSchema = new mongoose.Schema({
+  // User email
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Неверный формат почты',
+    },
+  },
+  // User password
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  // User name
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
