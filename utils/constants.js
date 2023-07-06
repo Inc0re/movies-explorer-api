bd89ccf0b7ace959cc882846ec2ab0687e214b50
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-06 18:46:36 +0600

    remove adminId, add temp * cors origin

--- utils/constants.js
+++ utils/constants.js
@@ -14,10 +14,9 @@ module.exports = {
   saltRounds: 10,
   jwtSecret: JWT_SECRET,
   urlTemplate: /(https?:\/\/)(www\.)?([a-z\d-]+\.)+([a-z]{2,6})(\/[A-Za-z\d\-._~:/?#[\]@!$&'()*+,;=]+)*\/?$/,
-  adminId: '64a68a1abaed3cc1784d7a9a',
-  mongoDBpath: 'mongodb://localhost:27017/moviesexplorerdb',
+  mongoDBpath: 'mongodb://localhost:27017/bitfilmsdb',
   corsOrigins: [
     'http://localhost:3000',
-    // !!! add production domain
+    '*', // !!! replace with production domain
   ],
 };
