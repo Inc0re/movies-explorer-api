4e6d67340c358379b2bb2d5b91c3370ceff7c4df
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-06 17:50:09 +0600

    use adminId from constants

--- middlewares/check-movie-owner.js
+++ middlewares/check-movie-owner.js
@@ -1,20 +1,21 @@
 const Movie = require('../models/movie');
+// !!! temp admin id
+const { adminId } = require('../utils/constants');
 const {
   BadRequestError,
   ForbiddenError,
   NotFoundError,
 } = require('../utils/errors');
 
-// !!! temp admin id
-const adminID = '64a68a1abaed3cc1784d7a9a';
-
 module.exports = (req, res, next) => {
   Movie.findById(req.params.movieId)
     .then((movie) => {
       if (!movie) {
         next(new NotFoundError(`Нет карточки с id ${req.params.movieId}`));
-        // !!! temp admin id
-      } else if (movie.owner.toString() !== req.user._id && req.user._id !== adminID) {
+      } else if (
+        movie.owner.toString() !== req.user._id
+        && req.user._id !== adminId // !!! temp admin id
+      ) {
         next(new ForbiddenError('Нет прав на удаление'));
       } else {
         next();
