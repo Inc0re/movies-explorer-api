3c053a0b37c23108ff646f6cfa7ff7e94b47537a
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-07 16:33:30 +0600

    add API start page

---
+++ utils/api-start-page.js
@@ -0,0 +1,16 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Films Explorer API</title>
+  </head>
+  <body>
+    <h1>Films Explorer API</h1>
+    <p>API for the Films Explorer project.</p>
+    <p>
+      API documentation is available
+      <a href="https://github.com/Inc0re/movies-explorer-api">here</a>
+    </p>
+  </body>
+</html>`;
