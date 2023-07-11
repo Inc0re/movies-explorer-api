7968da2cbdc7c21818f774d4271049a198831eba
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-11 22:37:20 +0600

    refactor: move all routes from app to routes/index

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const { celebrate, errors } = require('celebrate');
-const { login, createUser, logOut } = require('./controllers/users');
-const usersRouter = require('./routes/users');
-const moviesRouter = require('./routes/movies');
-const auth = require('./middlewares/auth');
-const checkAdminFlag = require('./middlewares/check-admin-flag');
+const { errors } = require('celebrate');
+const router = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/limiter');
-const userValidator = require('./utils/validators/userValidator');
 const { mongoDBpath, corsOrigins } = require('./utils/constants');
-const { NotFoundError } = require('./utils/errors');
-const apiStartPage = require('./utils/api-start-page');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -47,30 +40,8 @@ mongoose.connect(mongoDBpath, {
 // Request logger
 app.use(requestLogger);
 
-// API start page
-app.get('/', (req, res) => {
-  res.send(apiStartPage);
-});
-
-// Unprotected routes
-app.post('/signin', celebrate(userValidator.createOrLogin), login);
-app.post('/signup', celebrate(userValidator.createOrLogin), createUser);
-
-// Logout
-app.get('/logout', logOut);
-
-// Protected routes
-app.use('/users', auth, usersRouter);
-app.use('/movies', auth, moviesRouter);
-// Crash test app
-app.get('/crash', auth, checkAdminFlag, () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-app.use(auth, (req, res, next) => {
-  next(new NotFoundError('Запрашиваемый ресурс не найден'));
-});
+// Routes
+app.use(router);
 
 // Error logger
 app.use(errorLogger);
